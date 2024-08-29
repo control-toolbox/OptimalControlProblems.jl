293b5a5430f1bd0d1c9bb1eee9a15428a52908fb
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-08-29 03:08:45 +0200

    setup module

--- src/OptimalControlProblems.jl
+++ src/OptimalControlProblems.jl
@@ -2,4 +2,9 @@ module OptimalControlProblems
 
 greet() = print("Hello World!")
 
+include("OptimalControlModels/OptimalControlModels.jl")
+include("JuMPModels/JuMPModels.jl")
+
+export JuMPModels, OptimalControlModels
+
 end
