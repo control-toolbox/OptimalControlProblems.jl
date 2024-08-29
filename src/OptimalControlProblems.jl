1bbdf9ed6a98751dee399e3afc4d1d4f38a594cf
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-08-29 10:21:19 +0200

    add packages

--- src/OptimalControlProblems.jl
+++ src/OptimalControlProblems.jl
@@ -1,10 +1,9 @@
 module OptimalControlProblems
 
-greet() = print("Hello World!")
-
-include("OptimalControlModels/OptimalControlModels.jl")
-include("JuMPModels/JuMPModels.jl")
+include("./OptimalControlModels/OptimalControlModels.jl")
+include("./JuMPModels/JuMPModels.jl")
 
 export JuMPModels, OptimalControlModels
+
 
 end
