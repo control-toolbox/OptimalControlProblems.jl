6b75c342d85958b28ca3b0f8bbcbc912918d1d12
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 13:59:08 +0200

    set test

--- test/test_JuMP_optimality.jl
+++ test/test_JuMP_optimality.jl
@@ -13,7 +13,7 @@ function test_JuMP_optimality()
     set_optimizer_attribute(model,"mu_strategy","adaptive")
     set_optimizer_attribute(model,"linear_solver","mumps")
     # Solve the model
-    Ipopt.optimize!(model)
+    optimize!(model)
     # Test that the solver found an optimal solution
     @test termination_status(model) == MOI.OPTIMAL
 end
