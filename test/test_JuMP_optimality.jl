c6e410cd1058573c68584069ae9c7b33bb212f0d
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 14:42:17 +0200

    test on JuMP models

--- test/test_JuMP_optimality.jl
+++ test/test_JuMP_optimality.jl
@@ -11,18 +11,20 @@ function test_JuMP_optimality()
                         !(x in [:eval, :include]), 
                         all_names)
     for f in functions_list
-        # Set up the model
-        model = OptimalControlProblems.JuMPModels.eval(f)()
-        set_optimizer(model,Ipopt.Optimizer)
-        set_silent(model)
-        set_optimizer_attribute(model,"tol",1e-8)
-        set_optimizer_attribute(model,"constr_viol_tol",1e-6)
-        set_optimizer_attribute(model,"max_iter",1000)
-        set_optimizer_attribute(model,"mu_strategy","adaptive")
-        set_optimizer_attribute(model,"linear_solver","mumps")
-        # Solve the model
-        optimize!(model)
-        # Test that the solver found an optimal solution
-        @test termination_status(model) == MOI.LOCALLY_SOLVED
+        @testset "$(f)" begin
+            # Set up the model
+            model = OptimalControlProblems.JuMPModels.eval(f)()
+            set_optimizer(model,Ipopt.Optimizer)
+            set_silent(model)
+            set_optimizer_attribute(model,"tol",1e-8)
+            set_optimizer_attribute(model,"constr_viol_tol",1e-6)
+            set_optimizer_attribute(model,"max_iter",1000)
+            set_optimizer_attribute(model,"mu_strategy","adaptive")
+            set_optimizer_attribute(model,"linear_solver","mumps")
+            # Solve the model
+            optimize!(model)
+            # Test that the solver found an optimal solution
+            @test termination_status(model) == MOI.LOCALLY_SOLVED
+        end
     end    
 end
