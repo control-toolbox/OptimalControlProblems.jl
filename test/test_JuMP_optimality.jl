619525414118cec0a90f3e58cb5960d476b72fa4
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 14:35:43 +0200

    test on all JuMP models

--- test/test_JuMP_optimality.jl
+++ test/test_JuMP_optimality.jl
@@ -3,17 +3,26 @@ using JuMP
 
 # test_JuMP_optimality
 function test_JuMP_optimality()
-    # Set up the model
-    model = OptimalControlProblems.JuMPModels.cart_pendulum()
-    set_optimizer(model,Ipopt.Optimizer)
-    set_silent(model)
-    set_optimizer_attribute(model,"tol",1e-8)
-    set_optimizer_attribute(model,"constr_viol_tol",1e-6)
-    set_optimizer_attribute(model,"max_iter",1000)
-    set_optimizer_attribute(model,"mu_strategy","adaptive")
-    set_optimizer_attribute(model,"linear_solver","mumps")
-    # Solve the model
-    optimize!(model)
-    # Test that the solver found an optimal solution
-    @test termination_status(model) == MOI.LOCALLY_SOLVED
+    # Collecting all the OptimalControlProblems.JuMPModels models
+    all_names = names(OptimalControlProblems.JuMPModels, all=true)
+    functions_list = filter(x -> isdefined(OptimalControlProblems.JuMPModels, x) && 
+                        isa(getfield(OptimalControlProblems.JuMPModels, x), Function) &&
+                        !startswith(string(x), "#") && 
+                        !(x in [:eval, :include]), 
+                        all_names)
+    for f in functions_list
+        # Set up the model
+        model = OptimalControlProblems.JuMPModels.eval(f)()
+        set_optimizer(model,Ipopt.Optimizer)
+        set_silent(model)
+        set_optimizer_attribute(model,"tol",1e-8)
+        set_optimizer_attribute(model,"constr_viol_tol",1e-6)
+        set_optimizer_attribute(model,"max_iter",1000)
+        set_optimizer_attribute(model,"mu_strategy","adaptive")
+        set_optimizer_attribute(model,"linear_solver","mumps")
+        # Solve the model
+        optimize!(model)
+        # Test that the solver found an optimal solution
+        @test termination_status(model) == MOI.LOCALLY_SOLVED
+    end    
 end
