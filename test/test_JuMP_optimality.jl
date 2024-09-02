df20278660fecea86b173bc4f34e0af46e76d5e3
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 13:17:18 +0200

    first test on JuMP

---
+++ test/test_JuMP_optimality.jl
@@ -0,0 +1,8 @@
+# test_JuMP_optimality
+function test_JuMP_optimality()
+    model = OptimalControlProblems.JuMPModels.cart_pendulum()
+    # Solve the model
+    optimize!(model)
+    # Test that the solver found an optimal solution
+    @test termination_status(model) == MOI.OPTIMAL
+end
