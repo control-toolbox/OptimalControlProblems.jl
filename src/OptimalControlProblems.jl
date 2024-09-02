2648a9b70ace1c0b8f3fea9527423ce66c0e1a0e
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 10:45:34 +0200

    Update OptimalControlProblems.jl

--- src/OptimalControlProblems.jl
+++ src/OptimalControlProblems.jl
@@ -7,4 +7,42 @@ export JuMPModels, OptimalControlModels
 
 greet() = print("Hello World!")
 
+path = dirname(@__FILE__)
+files = filter(x -> x[(end - 2):end] == ".jl", readdir(path * "/MetaData"))
+for file in files
+  include("MetaData/" * file)
+end
+nb_problems = length(files)
+
+const infos = [
+    :name
+    :nvar
+    :ncon
+    :minimize
+    :objective_value
+]
+
+const types = [
+    String
+    Int
+    Int
+    Bool
+    Real
+]
+
+"""
+The following keys are valid:
+    - `name::String`: problem name
+    - `nvar::Int`: number of variables
+    - `ncon::Int`: number of general constraints
+    - `minimize::Bool`: true if optimize == minimize
+    - `objective_value::Real`: objective value
+"""
+const meta = DataFrame(infos .=> [Array{T}(undef, number_of_problems) for T in types])
+
+for name in names, i = 1:number_of_problems
+  meta[!, name][i] = eval(Meta.parse("$(split(files[i], ".")[1])_meta"))[name]
+end
+
+
 end
