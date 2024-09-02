9289696eed71b8d573a2b818d5527341b6b7f9b2
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-02 15:11:38 +0200

    add OC tests

---
+++ test/test_OptimalControl_optimality.jl
@@ -0,0 +1,27 @@
+using OptimalControl
+using NLPModelsIpopt
+
+# test_OptimalControl_optimality
+function test_OptimalControl_optimality()
+    nh=100
+    # Collecting all the OptimalControlProblems.OptimalControlModels models
+    all_names = names(OptimalControlProblems.OptimalControlModels, all=true)
+    all_functions_list = filter(x -> isdefined(OptimalControlProblems.OptimalControlModels, x) && 
+                        isa(getfield(OptimalControlProblems.OptimalControlModels, x), Function) &&
+                        !startswith(string(x), "#") && 
+                        !(x in [:eval, :include]), 
+                        all_names)
+    functions_list = filter(x -> !endswith(string(x), "_init"), all_functions_list)
+    init_functions_list = filter(x -> endswith(string(x), "_init"), all_functions_list)
+    for f in functions_list
+        @testset "$(f)" begin
+            # Set up the model
+            model = OptimalControlProblems.OptimalControlModels.eval(f)()
+            f_init = init_functions_list[findfirst(x -> x == Symbol(f , "_init"), init_functions_list)]
+            init = OptimalControlProblems.OptimalControlModels.eval(f_init)(;nh=nh)
+            sol = OptimalControl.solve(model, init=init, grid_size = nh)
+            # Test that the solver found an optimal solution
+            @test sol.message == "Solve_Succeeded"
+        end
+    end    
+end
