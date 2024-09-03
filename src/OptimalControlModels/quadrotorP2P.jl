8b0fbb9fccc75500f02abb2f35d0530ff0782ac1
H.Yassine <90392644+0Yassine0@users.noreply.github.com>
2024-09-03 11:32:29 +0200

    add OC full list

---
+++ src/OptimalControlModels/quadrotorP2P.jl
@@ -0,0 +1,116 @@
+"""
+Quadrotor Problem:
+    We want to find the optimal trajectory of a quadrotor to reach a target position.
+    The objective is to minimize the final time.
+    The problem is formulated as an OptimalControl model.
+"""
+function quadrotorP2P()
+    # parameters
+    g = 9.81
+    atmin = 0
+    atmax = 9.18*5
+    tiltmax = 1.1 / 2
+    dtiltmax = 6.0 / 2
+    p0 = [0.0, 0.0, 2.5]
+    v0 = [0, 0, 0]
+    u0 = [9.81, 0, 0, 0]
+    pf = [0.01, 5.0, 2.5]
+    vf = [0.0, 0.0, 0.0]
+
+    @def ocp begin
+        ## parameters
+        g = 9.81
+        atmin = 0
+        atmax = 9.18*5
+        tiltmax = 1.1 / 2
+        dtiltmax = 6.0 / 2
+        p0 = [0.0, 0.0, 2.5]
+        v0 = [0, 0, 0]
+        u0 = [9.81, 0, 0, 0]
+        pf = [0.01, 5.0, 2.5]
+        vf = [0.0, 0.0, 0.0]
+
+        ## define the problem
+        tf ∈ R¹, variable
+        t ∈ [ 0.0, tf ], time
+        x ∈ R⁸, state
+        u ∈ R⁴, control
+
+        ## state variables
+        p1 = x₁
+        p2 = x₂
+        p3 = x₃
+        v1 = x₄
+        v2 = x₅
+        v3 = x₆
+        ϕ = x₇
+        θ = x₈
+        ## control variables
+        at = u₁
+        ϕ_dot = u₂
+        θ_dot = u₃
+        ψ = u₄
+
+        ## constraints
+        # state constraints
+        tf ≥ 0.0,                                       (tf_con)
+        # control constraints
+        -pi/2 ≤ ϕ(t) ≤ pi/2,                            (ϕ_con) 
+        -pi/2 ≤ θ(t) ≤ pi/2,                            (θ_con)
+        cos(θ(t))*cos(ϕ(t)) ≥ cos(tiltmax),             (tiltmax_con)
+        -dtiltmax ≤ ϕ_dot(t)   ≤ dtiltmax,              (ϕdot_con)
+        -dtiltmax ≤ θ_dot(t)  ≤ dtiltmax,               (θdot_con)
+        atmin ≤ at(t) ≤ atmax,                          (at_con)
+        # initial constraints
+        p1(0) == p0[1],      (p1_i)
+        p2(0) == p0[2],      (p2_i)
+        p3(0) == p0[3],      (p3_i)
+        v1(0) == v0[1],      (v1_i)
+        v2(0) == v0[2],      (v2_i)
+        v3(0) == v0[3],      (v3_i)
+        ϕ(0) == u0[2],      (ϕ_i)
+        θ(0) == u0[3],      (θ_i)
+        # final constraints
+        p1(tf) == pf[1],     (p1_f)
+        p2(tf) == pf[2],     (p2_f)
+        p3(tf) == pf[3],     (p3_f)
+        v1(tf) == vf[1],   (v1_f)
+        v2(tf) == vf[2],   (v2_f)
+        v3(tf) == vf[3],   (v3_f)
+
+        ## dynamics
+        ẋ(t) == dynamics(x(t), u(t))
+
+        ## objective  
+        tf + ∫(1e-8 * (ϕ(t)^2 + θ(t)^2 +ψ(t)^2 + at(t)^2) + (1e2*(ψ(t)- u0[3])^2)) → min
+    end
+
+    function dynamics(x,u)
+        p1, p2, p3, v1, v2, v3, ϕ, θ = x
+        at , ϕ_dot, θ_dot, ψ = u
+
+        cr = cos(ϕ)
+        sr = sin(ϕ)
+        cp = cos(θ)
+        sp = sin(θ)
+        cy = cos(ψ)
+        sy = sin(ψ)
+        R=  [(cy*cp)    (cy*sp*sr - sy*cr) (cy*sp*cr + sy*sr);
+            (sy*cp)     (sy*sp*sr + cy*cr) (sy*sp*cr - cy*sr);
+            (-sp)       (cp*sr)            (cp*cr)]
+        at_ = R*[0;0;at]
+        g_ = [0;0;-g]
+        a = g_ + at_
+
+        return [v1, v2, v3, a[1], a[2], a[3], ϕ_dot, θ_dot]
+    end
+
+    return ocp
+end
+
+
+
+function quadrotorP2P_init(;nh)
+    init = (control = [10, 0.0, 0.0, 0.0],);
+    return init
+end
---
+++ src/OptimalControlModels/space_Shuttle.jl
@@ -0,0 +1,183 @@
+"""
+Space Shuttle Reentry Trajectory Problem:
+    We want to find the optimal trajectory of a space shuttle reentry.
+    The objective is to minimize the angle of attack at the terminal point.
+    The problem is formulated as an OptimalControl model.
+"""
+function space_shuttle()
+    ## Global variables
+    w = 203000.0  # weight (lb)
+    g₀ = 32.174    # acceleration (ft/sec^2)
+    m = w / g₀    # mass (slug)
+
+    ## Aerodynamic and atmospheric forces on the vehicle
+    ρ₀ = 0.002378
+    hᵣ = 23800.0
+    Rₑ = 20902900.0
+    μ = 0.14076539e17
+    S = 2690.0
+    a₀ = -0.20704
+    a₁ = 0.029244
+    b₀ = 0.07854
+    b₁ = -0.61592e-2
+    b₂ = 0.621408e-3
+    c₀ = 1.0672181
+    c₁ = -0.19213774e-1
+    c₂ = 0.21286289e-3
+    c₃ = -0.10117249e-5
+
+    ## Initial conditions
+    h_s = 2.6          # altitude (ft) / 1e5
+    ϕ_s = deg2rad(0)   # longitude (rad)
+    θ_s = deg2rad(0)   # latitude (rad)
+    v_s = 2.56         # velocity (ft/sec) / 1e4
+    γ_s = deg2rad(-1)  # flight path angle (rad)
+    ψ_s = deg2rad(90)  # azimuth (rad)
+    α_s = deg2rad(0)   # angle of attack (rad)
+    β_s = deg2rad(0)   # bank angle (rad)
+    t_s = 1.00         # time step (sec)
+
+    ## Final conditions, the so-called Terminal Area Energy Management (TAEM)
+    h_t = 0.8          # altitude (ft) / 1e5
+    v_t = 0.25         # velocity (ft/sec) / 1e4
+    γ_t = deg2rad(-5)  # flight path angle (rad)
+    tf = 2009.0 # final time (sec)
+    t0 = 0.0 # initial time (sec)
+
+    @def ocp begin 
+        ## parameters
+        w = 203000.0  # weight (lb)
+        g₀ = 32.174    # acceleration (ft/sec^2)
+        m = w / g₀    # mass (slug)
+        ρ₀ = 0.002378
+        hᵣ = 23800.0
+        Rₑ = 20902900.0
+        μ = 0.14076539e17
+        S = 2690.0
+        a₀ = -0.20704
+        a₁ = 0.029244
+        b₀ = 0.07854
+        b₁ = -0.61592e-2
+        b₂ = 0.621408e-3
+        c₀ = 1.0672181
+        c₁ = -0.19213774e-1
+        c₂ = 0.21286289e-3
+        c₃ = -0.10117249e-5
+        h_s = 2.6          # altitude (ft) / 1e5
+        ϕ_s = deg2rad(0)   # longitude (rad)
+        θ_s = deg2rad(0)   # latitude (rad)
+        v_s = 2.56         # velocity (ft/sec) / 1e4
+        γ_s = deg2rad(-1)  # flight path angle (rad)
+        ψ_s = deg2rad(90)  # azimuth (rad)
+        α_s = deg2rad(0)   # angle of attack (rad)
+        β_s = deg2rad(0)   # bank angle (rad)
+        t_s = 1.00 
+        h_t = 0.8          # altitude (ft) / 1e5
+        v_t = 0.25         # velocity (ft/sec) / 1e4
+        γ_t = deg2rad(-5)  # flight path angle (rad)
+        tf = 2012.0 # final time (sec)
+        t0 = 0.0 # initial time (sec)
+
+        ## define the problem
+        t ∈ [ t0, tf ], time
+        x ∈ R⁶, state
+        u ∈ R², control
+
+        ## state variables
+        scaled_h = x₁
+        ϕ = x₂
+        θ = x₃
+        scaled_v = x₄
+        γ = x₅
+        ψ = x₆
+
+        ## control variables
+        α = u₁
+        β = u₂
+
+        ## constraints
+        # variable constraints
+        # state constraints
+        scaled_h(t) ≥ 0,                        (scaled_h_con)
+        deg2rad(-89) ≤ θ(t) ≤ deg2rad(89),      (θ_con)
+        scaled_v(t) ≥ 1e-4,                     (scaled_v_con)
+        deg2rad(-89) ≤ γ(t) ≤ deg2rad(89),      (γ_con)
+        # control constraints
+        deg2rad(-89) ≤ β(t) ≤ deg2rad(1),       (β_con)
+        deg2rad(-90) ≤ α(t) ≤ deg2rad(90),      (α_con)
+        # initial conditions
+        scaled_h(t0) == h_s,                    (scaled_h0_con)
+        ϕ(t0) == ϕ_s,                           (ϕ0_con)
+        θ(t0) == θ_s,                           (θ0_con)
+        scaled_v(t0) == v_s,                    (scaled_v0_con)
+        γ(t0) == γ_s,                           (γ0_con)
+        ψ(t0) == ψ_s,                           (ψ0_con)
+        # final conditions
+        scaled_h(tf) == h_t,                    (scaled_hf_con)
+        scaled_v(tf) == v_t,                    (scaled_vf_con)
+        γ(tf) == γ_t,                           (γf_con)
+
+        ## dynamics  
+        ẋ(t) == dynamics(x(t),u(t))
+
+        ## objective
+        θ(tf) → max
+    end
+
+    ## dynamics
+    function dynamics(x,u)
+        scaled_h, ϕ, θ, scaled_v, γ, ψ = x
+        α, β = u
+        h = scaled_h * 1e5
+        v = scaled_v * 1e4
+        ## Helper functions
+        c_D = b₀ + b₁ * rad2deg(α) + b₂ * (rad2deg(α)^2)
+        c_L = a₀ + a₁ * rad2deg(α)
+        ρ = ρ₀ * exp(-h/hᵣ)
+        D = (1/2) * c_D * S * ρ * (v^2)
+        L = (1/2) * c_L * S * ρ * (v^2)
+        r = Rₑ + h
+        g = μ / (r^2)
+
+        ## dynamics  
+        h_dot = v * sin(γ)
+        ϕ_dot = (v/r) * cos(γ) * sin(ψ) / cos(θ)
+        θ_dot = (v/r) * cos(γ) * cos(ψ)
+        v_dot = -(D/m) - g*sin(γ)
+        γ_dot = (L/(m*v)) * cos(β) + cos(γ) * ((v/r)-(g/v))
+        ψ_dot = (1/(m*v*cos(γ))) * L*sin(β) + (v/(r*cos(θ))) * cos(γ) * sin(ψ) * sin(θ)
+
+        return [ h_dot, ϕ_dot, θ_dot, v_dot, γ_dot, ψ_dot]
+    end
+
+    return ocp
+end
+
+
+function space_shuttle_init(;nh)
+    # Helper function for linear interpolation
+    function linear_interpolate(x_s, x_t, n)
+        return [x_s + (i-1) / (n-1) * (x_t - x_s) for i in 1:n]
+    end
+    # Interpolate each parameter separately
+    h_interp = linear_interpolate(h_s, h_t, nh)
+    ϕ_interp = linear_interpolate(ϕ_s, ϕ_s, nh) # no change in longitude
+    θ_interp = linear_interpolate(θ_s, θ_s, nh) # no change in latitude
+    v_interp = linear_interpolate(v_s, v_t, nh)
+    γ_interp = linear_interpolate(γ_s, γ_t, nh)
+    ψ_interp = linear_interpolate(ψ_s, ψ_s, nh) # no change in azimuth
+    α_interp = linear_interpolate(α_s, α_s, nh) # no change in angle of attack
+    β_interp = linear_interpolate(β_s, β_s, nh) # no change in bank angle
+    t_interp = linear_interpolate(t_s, t_s, nh) # no change in time step
+    # Combine all interpolated parameters into an array of arrays
+    interpolated_values = [transpose([h, ϕ, θ, v, γ, ψ, α, β, t]) for (h, ϕ, θ, v, γ, ψ, α, β, t) in 
+                            zip(h_interp, ϕ_interp, θ_interp, v_interp, γ_interp, ψ_interp, α_interp, β_interp, t_interp)]
+    # Create the initial guess by summing the interpolated values
+    initial_guess = reduce(vcat, interpolated_values)
+
+    x_init = [initial_guess[i,1:6] for i in 1:nh];
+    u_init = [initial_guess[i,7:8] for i in 1:nh];
+    time_vec = LinRange(0.0,t_s*n*4,nh)
+    init = (time= time_vec, state= x_init, control= u_init)
+    return init
+end
