d237576f2e1ee74654f0c2b594b79fcb5b9b240b
Olivier Cots <olivier.cots@irit.fr>
2025-10-01 23:01:50 +0200

    foo

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,7 +56,7 @@ const VERBOSE = true # print or not details during tests
         :OptimalControl_s,      # convergence tests for OptimalControl models
         :init,                  # comparison between OptimalControl and JuMP: init
         # :solution,              # comparison between OptimalControl and JuMP: solution
-        :quick,                 # quick comparison: objective rel error only
+        # :quick,                 # quick comparison: objective rel error only
         :parameters,            # tests with different parameters values, does no depend on `list_of_problems`
     )
         @testset "$(name)" verbose=VERBOSE begin
