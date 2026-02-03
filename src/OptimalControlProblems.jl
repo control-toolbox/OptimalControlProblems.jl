c1fab4f87dc9f4bbb1c2dd760169df11ddb618fc
Amiel <amielmetier@gmail.com>
2026-02-03 09:20:17 +0100

    add space shuttle

--- src/OptimalControlProblems.jl
+++ src/OptimalControlProblems.jl
@@ -69,7 +69,7 @@ function make_list_of_problems()
         :moonlander,
         :ducted_fan,
         :robot,
-        :space_shuttle,
+        #:space_shuttle,
     ]
     list_of_problems = setdiff(list_of_problems, problems_to_exclude)
 
