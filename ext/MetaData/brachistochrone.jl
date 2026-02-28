461afdf825aff66fee0ac7f537f12afba94585b5
Amiel <amielmetier@gmail.com>
2026-02-28 22:20:38 +0100

    add brachistochrone

---
+++ ext/MetaData/brachistochrone.jl
@@ -0,0 +1,12 @@
+brachistochrone_meta = OrderedDict(
+    :grid_size => 500,                 
+    :parameters => (
+        g  = 9.80665,                 
+        t0 = 0.0,                      
+        x0 = 0.0,                      
+        y0 = 10.0,                     
+        v0 = 0.0,                    
+        xf = 10.0,                    
+        yf = 5.0,                     
+    ),
+)
